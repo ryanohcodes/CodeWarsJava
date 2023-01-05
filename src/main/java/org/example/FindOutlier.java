0eaba4d8e06b68624eb64a34c3fb1eb509924dc3
Ryan Oh <ryanohnoes@gmail.com>
2023-01-05 10:08:33 -0800

    Add solution to 6 kyu "Find The Parity Outlier"

---
+++ src/main/java/org/example/FindOutlier.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Arrays;
+public class FindOutlier{
+    static int find(int[] integers){
+        int[] oddArray = Arrays.stream(integers).filter(x -> Math.abs(x) % 2 == 1).toArray();
+        int[] evenArray = Arrays.stream(integers).filter(x -> x % 2 == 0).toArray();
+        int ans;
+        if(oddArray.length > evenArray.length){
+            ans = evenArray[0];
+        }else{
+            ans = oddArray[0];
+        }
+        return ans;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,17 +6,19 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        String word = Integer.toString(493193);
-        while(word.length() > 1){
-            String[] current = word.split("");
-            int number = Arrays.stream(current).map(Integer::parseInt).reduce(0, Integer::sum);
-            word = Integer.toString(number);
-        }
-        System.out.println(word.getClass().getSimpleName().equals("String"));
+//        String word = Integer.toString(493193);
+//        while(word.length() > 1){
+//            String[] current = word.split("");
+//            int number = Arrays.stream(current).map(Integer::parseInt).reduce(0, Integer::sum);
+//            word = Integer.toString(number);
+//        }
+//        System.out.println(word.getClass().getSimpleName().equals("String"));
         //testing zone
-        SpinWords ans = new SpinWords();
-        System.out.println(ans.spinWords("Hey fellow warriors"));
-        System.out.println(XO.getXO("xxoo"));
+//        SpinWords ans = new SpinWords();
+//        System.out.println(ans.spinWords("Hey fellow warriors"));
+//        System.out.println(XO.getXO("xxoo"));
+        System.out.println(FindOutlier.find(new int[]{206847684,1056521,7,17,1901,21104421,7,1,35521,1,7781}));
+
     }
 
 
