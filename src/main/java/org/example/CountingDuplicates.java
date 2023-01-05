32a00c53cbd85109d66eff2fd67602c469c29f61
Ryan Oh <ryanohnoes@gmail.com>
2023-01-04 22:23:43 -0800

    Trying commit with intellij

---
+++ src/main/java/org/example/CountingDuplicates.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.HashMap;
+public class CountingDuplicates {
+    public static int duplicateCount(String text) {
+        // Write your code here
+        int counter = 0;
+        text = text.toLowerCase();
+        HashMap<Character,Integer> ans = new HashMap<>();
+        for(int i = 0; i < text.length();i++){
+            char current = text.charAt(i);
+            if(ans.get(current) == null){
+                ans.put(current,1);
+            }else if(ans.get(current) == 1){
+                counter++;
+                ans.put(current,ans.get(current)+1);
+            }
+        }
+        return counter;
+    }
+}
