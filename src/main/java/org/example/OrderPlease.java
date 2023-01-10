67f164d78479a8d5850dd31283b5cab7e86f5bb9
Ryan Oh <ryanohnoes@gmail.com>
2023-01-10 10:26:20 -0800

    Add solution to Your order, please (6 kyu)

---
+++ src/main/java/org/example/OrderPlease.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class OrderPlease {
+    public static String order(String words) {
+        if(words.isEmpty()) return "";
+        //split into string[]
+        String[] ans = words.split(" ");
+        //iterate over that array into a new array
+        String[] fina = new String[ans.length];
+        for(int i = 0; i < ans.length;i++){
+            for(int j = 0; j < ans[i].length();j++){
+                if(ans[i].charAt(j) >= 49 && ans[i].charAt(j) <= 57){
+                    int pos = Character.getNumericValue(ans[i].charAt(j));
+                    fina[pos-1] = ans[i];
+                }
+            }
+        }
+        //join
+        return String.join(" ",fina);
+    }
+}
