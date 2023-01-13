dcead12fffd236e0b74544cc5f18f54e9636cfe6
Ryan Oh <ryanohnoes@gmail.com>
2023-01-13 10:33:13 -0800

    Add solution to Does my number look big in this? (6 kyu)

---
+++ src/main/java/org/example/NumberUtils.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class NumberUtils {
+
+    public static boolean isNarcissistic(int number) {
+        //get length of the number
+        String thisNum = Integer.toString(number);
+        int numberLength = thisNum.length();
+        //raise each number to that length
+        String[] splitString = thisNum.split("");
+        double total = 0;
+        for (String s : splitString) {
+            total += Math.pow(Double.parseDouble(s), numberLength);
+        }
+        //add the numbers
+        //see if it equals the original
+        return number == (int) total;
+    }
+
+}
---
+++ src/main/java/org/example/Xbonacci.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Xbonacci {
+
+    public double[] tribonacci(double[] s, int n) {
+        double[] ans = new double[n];
+        int len = Math.min(n, s.length);
+        System.arraycopy(s, 0, ans, 0, len);
+        for(int i =0; i < ans.length-3;i++){
+            ans[i+3] = ans[i] + ans[i+1] + ans[i+2];
+        }
+        return ans;
+    }
+}
